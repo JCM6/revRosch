f02cfce06af768a346a79323c2ce70511bc80f79
JCM6 <jcmoody6@gmail.com>
2019-04-16 16:00:20 -0700

    Update pythonDraw.py

--- pythonDraw.py
+++ pythonDraw.py
@@ -1,9 +1,16 @@
 import math
 import random
+import bpy
 
 #define iterations
 iterMax = 5
 iterCount = 0
+
+#define bounds
+maxRange = 10
+stepInc = 1
+startPoint = 0
+
 
 #define base components
 x = []
@@ -16,11 +23,18 @@ wy = []
 wz = []
 
 #component matricies
-pointMtrx = [x], [y], [z]
+pointMtrx = [x, y, z]
 wildPointMtrx = [wx], [wy], [wz]
 
 
 for i in range(iterMax):
-    x.append(random.randrange(1, 10, 1))
-print(x)
+    x.append(random.randrange(startPoint, maxRange, stepInc))
+
+for i in range(iterMax):
+    y.append(random.randrange(startPoint, maxRange, stepInc))
+
+for i in range(iterMax):
+    z.append(random.randrange(startPoint, maxRange, stepInc))
+
+print('matrix of pointvalues')
 print(pointMtrx)    
